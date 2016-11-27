fc00d1626be2d379b69bc1e5bc79a02d4c3930d6
akshat <awasthi.akshat@hotmail.com>
2016-11-27 17:47:30 +0530

    Changes classes to use autowiring and implicit bean discovery wherever possible. stream property of DragonSlayingQuest still uses java config.

--- src/main/java/com/dependencyinjectiontest/knights/BraveKnight.java
+++ src/main/java/com/dependencyinjectiontest/knights/BraveKnight.java
@@ -1,12 +1,13 @@
 package com.dependencyinjectiontest.knights;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("knight")
 public class BraveKnight implements Knight{
 	
+	@Autowired
 	private Quest quest;
-	
-	public BraveKnight(Quest quest) {
-		this.quest = quest;
-	}
 	
 	public void embarkOnQuest() {
 		quest.embark();
