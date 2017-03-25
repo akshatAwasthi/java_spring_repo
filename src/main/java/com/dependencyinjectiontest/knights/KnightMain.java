bb45121d5cd03ccf9685b385dad735354fb4e689
akshat <awasthi.akshat@hotmail.com>
2017-03-25 19:18:51 +0530

    Comments to describe changes needed for XML based or hybrid configuration.

--- src/main/java/com/dependencyinjectiontest/knights/KnightMain.java
+++ src/main/java/com/dependencyinjectiontest/knights/KnightMain.java
@@ -8,8 +8,11 @@ import com.dependencyinjectiontest.knights.config.KnightConfig;
 public class KnightMain {
 
 	public static void main(String[] args) throws BeansException {
+
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(KnightConfig.class);
-		// context.refresh();
+		// in case of xml based configuration, ^ line will change and load context from the xml file.
+		// beans and DI can be described in the xml or autowired like above by adding the following line
+		// to enable component scan of classes declared as @Component
 		Knight knight = (Knight) context.getBean("knightBean");
 		knight.embarkOnQuest();
 		context.close();
