3af4d3f35d75ef71988eee2c0bd19df03c4bd027
akshat <awasthi.akshat@hotmail.com>
2017-03-25 19:12:47 +0530

    InputStream autowiring fix in SavePrincessQuest bean.

--- src/main/java/com/dependencyinjectiontest/knights/KnightMain.java
+++ src/main/java/com/dependencyinjectiontest/knights/KnightMain.java
@@ -1,15 +1,11 @@
 package com.dependencyinjectiontest.knights;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.dependencyinjectiontest.knights.config.KnightConfig;
 
 public class KnightMain {
-
-	@Autowired
-	Knight knight;
 
 	public static void main(String[] args) throws BeansException {
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(KnightConfig.class);
--- src/main/java/com/dependencyinjectiontest/knights/SavePrincessQuest.java
+++ src/main/java/com/dependencyinjectiontest/knights/SavePrincessQuest.java
@@ -3,14 +3,12 @@ package com.dependencyinjectiontest.knights;
 import java.io.PrintStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("princessQuestBean")
 public class SavePrincessQuest implements Quest{
 
 	@Autowired
-	@Qualifier("streamBean")
 	private PrintStream stream;
 	
 	public void embark() {
